4953849b5c2a18b729512ab3cd67bdc6d0dbfc2e
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2022-08-10 09:36:00 +0100

    feat(calculateAge): endpoint to calculate age :sparkles:

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,34 @@
+import dayjs from 'dayjs';
 import dotenv from 'dotenv';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import express, { Express, Request, Response } from 'express';
 
 dotenv.config();
+dayjs.extend(relativeTime);
 
 const app: Express = express();
 const port = process.env.PORT || 4001;
 
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).json('Express + TypeScript Server');
+type RequestWithQuery = Request<{}, {}, {}, { dob: string }>;
+
+app.get('/', (_req: Request, res: Response) => {
+  res.status(200).json('TalentQL Pipeline Program Backend Assessment');
+});
+
+app.get('/howold', async (req: RequestWithQuery, res: Response) => {
+  const { dob } = req.query;
+
+  if (!dob) {
+    return res.status(400).json({ error: 'dob query param is required' });
+  }
+
+  if (isNaN(Number(dob))) {
+    return res
+      .status(400)
+      .json({ error: 'dob query param is not a valid timestamp' });
+  }
+
+  return res.status(200).json({ age: dayjs(Number(dob)).fromNow(true) });
 });
 
 app.listen(port, () => {
